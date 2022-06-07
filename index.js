2325a271e0dcf8ddfff17b3b2b210442398fe76b
Allan <kipropsallan@gmail.com>
2022-06-07 19:21:27 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+//no longer has DOM node
+
+const main = document.querySelector("main");
+main.remove();
+
+//'newHeader' variable that points to node 'h1#victory'
+
+const newHeader = document.createElement('h1');
+newHeader.id = "victory";
+
+//variable that points to node 'h1#victory' with "YOUR-NAME is the champion" inside
+
+newHeader.textContent ='Allan Kiprop is the champion'
